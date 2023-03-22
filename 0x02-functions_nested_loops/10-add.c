9f9fa04ae4917b0da40ca050413bf12e80c7871b
erradixahmed <erradi.hassar@gmail.com>
2023-03-22 09:17:16 -0700

    addy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Add two integers and returns result
+ * @a : 1st number
+ * @b : 2cnd number
+ * Return: the sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
