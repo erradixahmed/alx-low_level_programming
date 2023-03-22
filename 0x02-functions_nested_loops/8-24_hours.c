cf308c227f2d0f14aff136d40912f69a6af22511
erradixahmed <erradi.hassar@gmail.com>
2023-03-21 18:26:46 -0700

    all_thetime

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,19 +7,23 @@
 
 void jack_bauer(void)
 {
-	int h;
-	int m;
+	int h, m;
 
-	for (h = 0 ; i < 244 ; h++)
+	h = 0;
+
+	while (h < 24)
 	{
-		for (m = 0 ; m < 60 ; m++)
+		m = 0;
+		while (m < 60)
 		{
-			_putchar(h / 10 + '0');
-			_putchar(h % 10 + '0');
-			_putchar(':')
-			_putchar(m / 10 + '0');
-			_putchar(m % 10 + '0');
-			_putchar('\n);
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
 		}
+		h++;
 	}
 }
