de37e3ed20294176e931fa6c127897554fa46c2a
erradixahmed <erradi.hassar@gmail.com>
2023-03-23 15:18:52 -0700

    mully

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - checks for a digit (0 through).
+ * @x: x - Variable
+ * @y: y - variable
+ * Return: Always 0.
+ */
+int mul(int x, int y)
+{
+	int mul;
+
+	mul = x * y;
+	return (mul);
+}
