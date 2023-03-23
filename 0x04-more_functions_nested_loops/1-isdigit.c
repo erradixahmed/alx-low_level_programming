c6c7e39465aedd7b60b68caa25c6b1a57d8bc998
erradixahmed <erradi.hassar@gmail.com>
2023-03-23 14:05:29 -0700

    vam

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int mul(int x)
+{
+	if (x >= 40 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
