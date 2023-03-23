48631faddfc7c3c3f671431acad6fcdc748b9f4d
erradixahmed <erradi.hassar@gmail.com>
2023-03-23 13:23:14 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase char
+ * @x : var
+ * Return :0.
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x04
+more functions nested loops
+tasks
