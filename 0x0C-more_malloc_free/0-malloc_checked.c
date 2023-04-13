62a050b39862019e82d11c7028e7797b81ece3ab
erradixahmed <erradi.hassar@gmail.com>
2023-04-13 16:37:33 -0700

    my first

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,44 +2,20 @@
 #include "main.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated with a call to malloc
- * @old_size: size, in bytes, of the allocated space for ptr
- * @new_size: new size, in bytes, of the new memory block
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
  *
- * Return: pointer to the reallocated memory block
- *         NULL if new_size is zero and ptr is not NULL
- *         ptr if new_size is equal to old_size
- *         NULL if malloc fails or ptr is NULL
+ * Return: a pointer to the allocated memory
+ *         Exit with status value of 98 if malloc fails
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *malloc_checked(unsigned int b)
 {
-	void *new_ptr;
-	unsigned int i;
-
-	if (new_size == old_size)
-		return (ptr);
+	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-		return (malloc(new_size));
-
-	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr);
-		return (NULL);
+		exit(98);
 	}
 
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	for (i = 0; i < old_size && i < new_size; i++)
-		((char *)new_ptr)[i] = ((char *)ptr)[i];
-
-	free(ptr);
-
-	return (new_ptr);
+	return (ptr);
 }
