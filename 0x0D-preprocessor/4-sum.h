90e4347aa556395ba9edc6b846c0cb7663e50583
erradixahmed <erradi.hassar@gmail.com>
2023-05-02 07:02:26 -0700

    defere

--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,8 +0,0 @@
-/* guards against multiple inclusions */
-#ifndef SUM_H
-#define SUM_H
-
-/* function-like macro to compute the sum of two numbers */
-#define SUM(x, y) ((x) + (y))
-
-#endif /* SUM_H */
