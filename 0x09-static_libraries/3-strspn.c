a30363bda9217f01bd83a8503a8defe8d1bb0cd1
erradixahmed <erradi.hassar@gmail.com>
2023-06-09 12:56:12 +0100

    red and bleu

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ *
+ * @s: the initial segment of
+ *
+ * @accept: which consist only of bytes from
+ *
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				n++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -48,3 +48,4 @@ char *_strstr(char *haystack, char *needle);]
 
 
 [1-Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory]
+        
